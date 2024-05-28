fbcc4616a0c7d79cb8fc573a53bcd954e9d0ec5c
MatiasMartinez <matiaschino2012@gmail.com>
2024-05-28 14:40:13 -0300

    terminado

---
+++ js/validar-edad.js
@@ -0,0 +1,15 @@
+export default function esMayorDeEdad(campo){
+    const fechaDeNacimiento = new Date(campo.value);
+   
+    if(!validarEdad(fechaDeNacimiento)){
+        campo.setCustomValidity('Necesitas ser mayor de edad');
+    }
+}
+
+function validarEdad(fecha){
+    const fechaActual = new Date();
+    const fechaMas18 = new Date(fecha.getUTCFullYear()+18, fecha.getUTCMonth(), fecha.getUTCDate() );
+    console.log(fechaMas18);
+
+    return fechaActual >= fechaMas18;
+}
